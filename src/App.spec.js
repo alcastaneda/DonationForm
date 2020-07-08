e7fc2d4fd09ddb017dfd5d304fdf864103577e1f
Adriana <adriana.lcs316@gmail.com>
2020-07-08 17:15:24 -0500

    add test setup and tests for App.js

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,9 +1,14 @@
-import React from "react";
-// import renderer from "react-test-renderer";
-import { render } from "react-testing-library";
+import React from 'react';
+import { render } from 'react-testing-library';
 
-import App from "./App";
+import App from './App';
 
-it("should render without crashing", () => {
+it('should render without crashing', () => {
   render(<App />);
+});
+
+it('should match the snapshot', () => {
+  const { container } = render(<App />);
+
+  expect(container.firstChild).toMatchSnapshot();
 });
